d44fe516383992713437be99027e85be97ca6206
Joseph Moberly <jmobes15@gmail.com>
2021-07-02 23:29:05 -0700

    implemented binary search algorithm on sorted array.

---
+++ search/binary-search.js
@@ -0,0 +1,43 @@
+function binarySearch(arr, value) {
+  let left = 0;
+  let right = arr.length - 1;
+  let mid = Math.floor((left + right) / 2);
+
+  while (left <= right) {
+    if (value === arr[mid]) return mid;
+    else if (value < arr[mid]) right = mid - 1;
+    else left = mid + 1;
+    mid = Math.floor((left + right) / 2);
+  }
+  return -1;
+}
+console.log(
+  binarySearch(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    20
+  )
+);
+console.log(
+  binarySearch(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    100
+  )
+);
+console.log(
+  binarySearch(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    15
+  )
+);
+console.log(
+  binarySearch(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    -5
+  )
+);
+console.log(
+  binarySearch(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    9
+  )
+);
