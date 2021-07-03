60da4d9fd413177030ffc48ba842f4d01da45b78
Joseph Moberly <jmobes15@gmail.com>
2021-07-03 14:04:37 -0700

    implemented insertion sort algorithm.

---
+++ sort/insertion-sort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 50, 13, 421, 1, -43, 42, 10]));
+console.log(insertionSort([8, 1, 2, 3, 4]));
